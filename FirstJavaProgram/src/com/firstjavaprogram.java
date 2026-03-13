b2242c1d4c566118e1955fb807f8685763983afa
JamesPatzer <James.Patzer@uj.edu>
2026-03-13 17:07:20 -0500

    Update firstjavaprogram.java

--- FirstJavaProgram/src/com/firstjavaprogram.java
+++ FirstJavaProgram/src/com/firstjavaprogram.java
@@ -1,8 +1,10 @@
 public class firstjavaprogram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
+		System.out.println("Hello!");
+		System.out.println("This is my first Java Program!");
+		System.out.println("That is all folks!");
 
 	}
 
